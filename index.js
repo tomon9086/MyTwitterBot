99c162cad423c25f996acf59b028c654d71b36ee
tomon9086 <tomo.mouse.9086@gmail.com>
2017-10-20 01:46:28 +0900

    :sparkles:形態素解析までできた

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const OAuth = require("oauth")
+const kuromoji = require("kuromoji")
 
 const low = require("lowdb")
 const FileSync = require("lowdb/adapters/FileSync")
-const adapter = new FileSync("timeline.json")
-const db = low(adapter)
-db.defaults({ tweets: [] }).write()
+const tl_adapter = new FileSync("timeline.json")
+const timeline_db = low(tl_adapter)
+timeline_db.defaults({ tweets: [] }).write()
+const chains_adapter = new FileSync("chains.json")
+const markov_chain_db = low(chains_adapter)
+markov_chain_db.defaults({ lastAnalyzedID: null, chains: [] }).write()
 
 const oauth = new OAuth.OAuth(
 	"https://api.twitter.com/oauth/request_token",
@@ -42,7 +46,7 @@ const tweetForm = {
 }
 
 async function update(tl_count) {
-	const tweets = db.getState().tweets
+	const tweets = timeline_db.getState().tweets
 	const statuses = await getTimeline("4445069657", tl_count)
 	let indexofMaxId = 0
 	tweets.forEach(function(v, i) {
@@ -65,24 +69,35 @@ async function update(tl_count) {
 				}
 			}
 			if(v.retweeted_status !== undefined) tweet.isRetweet = true
-			db.get("tweets").push(tweet).write()
+			timeline_db.get("tweets").push(tweet).write()
 		}
-		// newTweets.sort(function(a, b) {
-		// 	return a.id - b.id
-		// })
 	})
-	// console.log(db.getState().tweets.length)
+	console.log("timelineとれたよ")
+	console.log(await sortTimelineJSON())
+	console.log("現在 " + timeline_db.getState().tweets.length + "ツイート")
+	await buildChainDB(timeline_db.getState().tweets)
+	// const generatedText = await makeTweet()
+	// await postStatus(generatedText)
+	// console.log(generatedText)
 }
 
 update(200)
-setInterval(() => {
-	update(100)
-}, 500000)
+// setInterval(() => {
+// 	update(100)
+// }, 500000)
 
 
 function compare(a, b) {
+	if(isNaN(Number(a)) || isNaN(Number(a))) return NaN
 	let a_larger_than_b = Number(a.slice(0, 9)) - Number(b.slice(0, 9)) === 0 ? a.slice(9, a.length) - b.slice(9, b.length) > 0 ? true : false : Number(a.slice(0, 9)) - Number(b.slice(0, 9)) > 0 ? true : false
 	return a_larger_than_b
+}
+
+function sortTimelineJSON() {
+	return new Promise(function(resolve, reject) {
+		timeline_db.get("tweets").sort(function(a, b) { return compare(a.id_str, b.id_str) * 2 - 1 }).write()
+		resolve("ソートしたよ")
+	})
 }
 
 function getTimeline(id, count) {
@@ -141,4 +156,79 @@ function postStatus(mes) {
 			}
 		)
 	})
+}
+
+function buildChainDB(data) {
+	const lastAnalyzedID = markov_chain_db.getState().lastAnalyzedID
+	return new Promise(async function(resolve, reject) {
+		await awaitForEach(data, function(v, i) {
+			if(lastAnalyzedID !== null && compare(lastAnalyzedID, v.id_str)) return
+			console.log(data.length + "ツイート中 " + (i + 1) + "番目を解析中...")
+			return new Promise(function(resolve, reject) {
+				kuromoji.builder({ dicPath: "./node_modules/kuromoji/dict" }).build(function(err, tokenizer) {
+					const path = tokenizer.tokenize(v.text)
+					// console.log(path)
+					path.reduce(function(p, c) {
+						let indexofWord = null
+						markov_chain_db.getState().chains.forEach(function(w, j) {
+							if(p.word_id === w.word_id) indexofWord = j
+						})
+						if(indexofWord !== null) {
+							let indexofNextWord = null
+							markov_chain_db.getState().chains[indexofWord].next_words.forEach(function(w, j) {
+								if(c.word_id === w.next_word_id) indexofNextWord = j
+							})
+							if(indexofNextWord !== null) {
+								markov_chain_db.getState().chains[indexofWord].next_words[indexofNextWord].count++
+								markov_chain_db.write()
+							} else {
+								const nextWord = {
+									next_word_id: c.word_id,
+									next_surface_form: c.surface_form,
+									next_pos: c.pos,
+									count: 1
+								}
+								markov_chain_db.getState().chains[indexofWord].next_words.push(nextWord)
+								markov_chain_db.write()
+							}
+						} else {
+							const newWord = {
+								word_id: p.word_id,
+								surface_form: p.surface_form,
+								pos: p.pos,
+								next_words: [{
+									next_word_id: c.word_id,
+									next_surface_form: c.surface_form,
+									next_pos: c.pos,
+									count: 1
+								}]
+							}
+							markov_chain_db.get("chains").push(newWord).write()
+						}
+						return c
+					})
+					resolve()
+				})
+				markov_chain_db.set("lastAnalyzedID", v.id_str).write()
+			})
+		})
+		resolve()
+	})
+}
+
+function awaitForEach(array, cb, i) {
+	return new Promise(async function(resolve, reject) {
+		if(i === undefined) i = 0
+		await cb(array[i], i, array)
+		if(i + 1 >= array.length) {
+			resolve()
+			return
+		}
+		awaitForEach(array, cb, i + 1)
+	})
+}
+
+function makeTweet() {
+	let text = ""
+	return text
 }
