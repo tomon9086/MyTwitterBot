78f8b16172ff99bd8656327790496b8a09a9ab96
tomon9086 <tomo.mouse.9086@gmail.com>
2017-10-20 19:40:35 +0900

    再帰だとpromiseがうまくいかないようだ

--- index.js
+++ index.js
@@ -72,19 +72,25 @@ async function update(tl_count) {
 			timeline_db.get("tweets").push(tweet).write()
 		}
 	})
-	console.log("timelineとれたよ")
-	console.log(await sortTimelineJSON())
-	console.log("現在 " + timeline_db.getState().tweets.length + "ツイート")
-	await buildChainDB(timeline_db.getState().tweets)
-	// const generatedText = await makeTweet()
-	// await postStatus(generatedText)
-	// console.log(generatedText)
+	// console.log("timelineとれたよ")
+	// console.log(await sortTimelineJSON())
+	// console.log("現在 " + timeline_db.getState().tweets.length + "ツイート")
+	// await buildChainDB(timeline_db.getState().tweets)
 }
 
-update(200)
-setInterval(() => {
-	update(100)
-}, 500000)
+async function postGeneratedTweet() {
+	const generatedText = await makeTweet()
+	// await postStatus(generatedText)
+	console.log("resolved: " + generatedText)
+}
+
+// update(200)
+// setInterval(() => {
+// 	update(100)
+// }, 500000)
+
+postGeneratedTweet()
+// setInterval(postGeneratedTweet, 1800000)
 
 
 function compare(a, b, opt) {
@@ -231,6 +237,58 @@ function awaitForEach(array, cb, i) {
 }
 
 function makeTweet() {
-	let text = ""
-	return text
+	console.log("ついーとつくるよ")
+	return new Promise(async function(resolve, reject) {
+		const chains = markov_chain_db.getState().chains
+		let text = ""
+		let initialWord = null
+		let isSuit = false
+		initialWord = chains[Math.random() * chains.length | 0]
+		// polymerize(initialWord.surface_form, initialWord)
+		text = await awaitPolymerize(chains, initialWord.surface_form, initialWord)
+		console.log(text)
+		resolve(text)
+	})
+}
+
+function polymerize(chains, text, word, count) {
+	return new Promise(function(resolve, reject) {
+		if(count === undefined) count = 0
+		if((Math.random() * (count / 2)) > 3) {
+			resolve(text)
+			return
+		}
+		let indexofWord = chains.indexOf(word)
+		if(indexofWord === -1) {
+			chains.forEach(function(v, i) {
+				if(word.next_word_id === v.word_id) {
+					indexofWord = i
+					return
+				}
+			})
+		}
+		// console.log(indexofWord)
+		if(indexofWord === -1) {
+			resolve(text)
+			return
+		}
+		const nextWords = chains[indexofWord].next_words
+		const nextWordCounts = nextWords.map(function(v, i) { return v.count })
+		const countSum = nextWordCounts.reduce(function(p, c) {
+			return p + c
+		})
+		const nextWordScore = nextWordCounts.map(function(v, i) { return Math.random() * v / countSum })
+		const maxScore = nextWordScore.reduce((p, c) => { return Math.max(p, c) })
+		const topScoreWords = []
+		nextWordScore.forEach(function(v, i) {
+			if(maxScore === v) topScoreWords.push(nextWords[i])
+		})
+		let pushedWord
+		if(topScoreWords.length === 1) {
+			pushedWord = topScoreWords[0]
+		} else if(topScoreWords.length > 1) {
+			pushedWord = topScoreWords[Math.random() * topScoreWords.length | 0]
+		}
+		polymerize(chains, text + pushedWord.next_surface_form, pushedWord, count + 1)
+	})
 }
